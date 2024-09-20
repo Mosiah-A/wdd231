bf96bb1e4ec58ce782769f0cbf698627ec79d9a0
Mosiah-A <mosiahassuncao@gmail.com>
2024-09-20 19:46:04 -0300

    updade activity

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('open');
+	hambutton.classList.toggle('open');
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+    // Get the last modified date
+    const lastModified = document.lastModified;
+
+    // Output the current year in the first paragraph of the footer
+    document.getElementById('currentYear').textContent = `© ${currentYear}`;
+
+    // Output the last modified date in the second paragraph of the footer
+    document.getElementById('last-modified').textContent = `Last modified: ${lastModified}`;
+});
